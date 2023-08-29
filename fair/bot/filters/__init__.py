b4e52f11614c5eb78e1592aea13d9e9901042c57
Cub11k <ostashenkokd@gmail.com>
2023-08-30 00:50:35 +0300

    Add text and callback_data filters

--- fair/bot/filters/__init__.py
+++ fair/bot/filters/__init__.py
@@ -2,6 +2,15 @@ from telebot import TeleBot
 from telebot.custom_filters import StateFilter
 
 
+from fair.bot.filters.callback_data import CallbackDataFilter, CallbackDataPaginationFilter
+from fair.bot.filters.text import TextEqualsFilter
+
+
 def add_custom_filters(bot: TeleBot):
     # add any custom filters here
     bot.add_custom_filter(StateFilter(bot))
+
+    bot.add_custom_filter(TextEqualsFilter())
+
+    bot.add_custom_filter(CallbackDataFilter())
+    bot.add_custom_filter(CallbackDataPaginationFilter())
