5cf1633b11190e4dee44c50917247eb82ea5e28b
Cub11k <ostashenkokd@gmail.com>
2023-09-05 11:27:23 +0300

    Player registration flow

--- fair/bot/handlers/__init__.py
+++ fair/bot/handlers/__init__.py
@@ -2,7 +2,10 @@ from telebot import TeleBot
 
 from fair.config import ButtonsConfig
 
+from fair.bot.handlers import (
+    player_registration_flow,
+)
+
 
 def register_handlers(bot: TeleBot, buttons: ButtonsConfig):
-    # register all handlers here
-    pass
+    player_registration_flow.register_handlers(bot)
