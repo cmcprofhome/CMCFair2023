d5a6acca0ed4fb3a3980187db0f473fe8cea661b
Cub11k <ostashenkokd@gmail.com>
2023-09-12 11:02:12 +0300

    Register handlers

--- fair/bot/handlers/__init__.py
+++ fair/bot/handlers/__init__.py
@@ -2,7 +2,11 @@ from telebot import TeleBot
 
 from fair.config import ButtonsConfig
 
+from fair.bot.handlers import (
+    manager_permanent_menu
+)
+
 
 def register_handlers(bot: TeleBot, buttons: ButtonsConfig):
     # register all handlers here
-    pass
+    manager_permanent_menu.register_handlers(bot, buttons)
