9eb4be642ca840d3119bf43b670d143684a94b5a
Cub11k <ostashenkokd@gmail.com>
2023-09-14 01:15:28 +0300

    Fix app context

--- fair/app.py
+++ fair/app.py
@@ -10,14 +10,14 @@ from fair.routes import setup_routes
 
 
 async def on_startup(app: Sanic):
-    bot = app.ctx['bot']
-    cfg = app.ctx['bot_config']
+    bot = app.ctx.bot
+    cfg = app.ctx.bot_config
     launch_bot(bot, cfg.drop_pending, cfg.use_webhook, cfg.allowed_updates, cfg.webhook)
 
 
 async def on_shutdown(app: Sanic):
-    bot = app.ctx['bot']
-    cfg = app.ctx['bot_config']
+    bot = app.ctx.bot
+    cfg = app.ctx.bot_config
     stop_bot(bot, cfg.use_webhook)
 
 
@@ -27,12 +27,12 @@ def build_app() -> Sanic:
     use_env_vars = os.environ.get('USE_ENV_VARS', True)
     config_env_mapping_path = os.environ.get('CONFIG_ENV_MAPPING_PATH', None)
     cfg = load_config(config_path, use_env_vars, config_env_mapping_path)
-    app.ctx['bot_config'] = cfg.bot
+    app.ctx.bot_config = cfg.bot
     db_logger = setup_logger(cfg.db.logger)
     db_adapter = setup_adapter(cfg.db, db_logger)
     bot_logger = setup_logger(cfg.bot.logger)
     bot = setup_bot(cfg.bot, db_adapter, cfg.messages, cfg.buttons, bot_logger)
-    app.ctx['bot'] = bot
+    app.ctx.bot = bot
 
     setup_routes(app)
     app.before_server_start(on_startup)
