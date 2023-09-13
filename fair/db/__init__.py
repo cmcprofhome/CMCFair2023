37f490755ead548578d94cecde44e2395d98b901
Cub11k <ostashenkokd@gmail.com>
2023-09-13 18:23:27 +0300

    Add launch via cli

--- fair/db/__init__.py
+++ fair/db/__init__.py
@@ -1,15 +1,15 @@
+from logging import Logger
+
 from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import sessionmaker
 
 from fair.config import DBConfig
-from fair.logger import setup_logger
 
 from fair.db.adapter import DBAdapter
 from fair.db.exceptions import DBError
 
 
-def setup_adapter(db_config: DBConfig):
-    db_logger = setup_logger(db_config.logger)
+def setup_adapter(db_config: DBConfig, logger: Logger):
     db_url = URL.create(
         drivername="postgresql+psycopg",
         username=db_config.user,
@@ -20,5 +20,5 @@ def setup_adapter(db_config: DBConfig):
     )
     db_engine = create_engine(db_url)
     db_session_maker = sessionmaker(bind=db_engine)
-    db_adapter = DBAdapter(session_maker=db_session_maker, logger=db_logger)
+    db_adapter = DBAdapter(session_maker=db_session_maker, logger=logger)
     return db_adapter
