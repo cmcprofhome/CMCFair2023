76e7606508d79db2681349cac9a5c1fca8ea49f6
Cub11k <ostashenkokd@gmail.com>
2023-09-14 09:55:53 +0300

    Try aiohttp

--- fair/app.py
+++ fair/app.py
@@ -1,26 +1,26 @@
 import os
 
-from sanic import Sanic
+from aiohttp.web import Application
 
-from fair.bot import setup_bot, launch_bot, stop_bot
+from fair.bot import setup_bot, launch_bot
 from fair.config import load_config
 from fair.db import setup_adapter
 from fair.logger import setup_logger
 from fair.routes import setup_routes
 
 
-def build_app() -> Sanic:
-    app = Sanic("FairBotApp")
+def build_app() -> Application:
+    app = Application()
     config_path = os.environ.get('CONFIG_PATH', './config.toml')
     use_env_vars = os.environ.get('USE_ENV_VARS', True)
     config_env_mapping_path = os.environ.get('CONFIG_ENV_MAPPING_PATH', None)
     cfg = load_config(config_path, use_env_vars, config_env_mapping_path)
-    app.ctx.bot_config = cfg.bot
+    app['bot_config'] = cfg.bot
     db_logger = setup_logger(cfg.db.logger)
     db_adapter = setup_adapter(cfg.db, db_logger)
     bot_logger = setup_logger(cfg.bot.logger)
     bot = setup_bot(cfg.bot, db_adapter, cfg.messages, cfg.buttons, bot_logger)
-    app.ctx.bot = bot
+    app['bot'] = bot
 
     setup_routes(app)
     launch_bot(bot, cfg.bot.drop_pending, cfg.bot.use_webhook, cfg.bot.allowed_updates, cfg.bot.webhook)
--- fair/routes.py
+++ fair/routes.py
@@ -1,18 +1,17 @@
-from sanic import Sanic, Request
-from sanic.response import text
+from aiohttp.web import Application, Request, Response
 from telebot.types import Update
 
 
 # TODO: move it to the separate file / module
 async def handle_telegram_update(request: Request):
-    secret_token = request.app.ctx.bot_config.webhook.secret_token
+    secret_token = request.app['bot_config'].webhook.secret_token
     if secret_token != request.headers.get('X-Telegram-Bot-Api-Secret-Token'):
-        return text('Forbidden', status=403)
+        return Response(body='Forbidden', status=403)
     body_json = await request.json()
-    bot = request.app.ctx.bot
+    bot = request.app['bot']
     bot.process_new_updates([Update.de_json(body_json)])
-    return text('OK')
+    return Response(body='OK', status=200)
 
 
-def setup_routes(app: Sanic):
-    app.add_route(handle_telegram_update, '/' + app.ctx.bot_config.token, methods=['POST'])
+def setup_routes(app: Application):
+    app.router.add_post('/' + app['bot_config'].token, handle_telegram_update)
